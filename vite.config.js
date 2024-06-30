f5662d645626d33591252585f6a83a8c7573e350
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-06-30 12:43:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     server: {
-    host: true, // or specify a hostname like 'localhost'
+    host: true, 
     strictPort: true,
     port: 5173,
   },
