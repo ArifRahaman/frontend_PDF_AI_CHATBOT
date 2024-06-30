f859196ee1445663d3c148f1dfb9a339519ca79a
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-06-30 12:14:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthContextProvider } from '../Context/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import 'regenerator-runtime/runtime';
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
