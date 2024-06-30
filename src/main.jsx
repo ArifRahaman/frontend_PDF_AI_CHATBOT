80728a1235a3ec667a80afdaad86180eca47906e
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-06-30 12:30:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthContextProvider } from '../Context/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import 'regenerator-runtime/runtime';
+// import 'regenerator-runtime/runtime';
 
 import './index.css'
 
